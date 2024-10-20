02097d12239cf76128569765d690f19628747917
DonizeteVida <donizetevida1998@outlook.com>
2024-10-20 01:05:33 -0300

    Right wheel works

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,11 @@
 #include <Arduino.h>
+#include <PwmOut.h>
+
+mbed::PwmOut R_A(PinName::p10);
+mbed::PwmOut R_B(PinName::p11);
+
+mbed::PwmOut L_A(PinName::p12);
+mbed::PwmOut L_B(PinName::p13);
 
 void setup()
 {
@@ -8,5 +15,13 @@ void setup()
 void loop()
 {
   Serial.println("Hello World");
+  R_B.write(1);
+  delay(1000);
+  R_B.write(0);
+  delay(1000);
+
+  R_A.write(1);
+  delay(1000);
+  R_A.write(0);
   delay(1000);
 }
